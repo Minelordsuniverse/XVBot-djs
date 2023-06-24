64d1411ed9a520d00f5a177d97aef36a0adb4f30
DefinitelyNotML <67429188+Minelordsuniverse@users.noreply.github.com>
2023-06-25 01:02:03 +0800

    Update deploy-commands.js

--- deploy-commands.js
+++ deploy-commands.js
@@ -10,7 +10,7 @@ const commandFolders = fs.readdirSync(foldersPath);
 
 for (const folder of commandFolders) {
 	// Grab all the command files from the commands directory you created earlier
-	const commandsPath = path.join(foldersPath, folder);
+	const commandsPath = path.join(__dirname, 'commands');
 	const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
 	// Grab the SlashCommandBuilder#toJSON() output of each command's data for deployment
 	for (const file of commandFiles) {
